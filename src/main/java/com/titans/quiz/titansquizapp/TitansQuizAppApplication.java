083894d3bf5f541e217ae9dd22f534ca1c24e842
rakeshmaruthuraj <rakeshmaruthuraj@gmail.com>
2021-06-29 11:35:47 -0700

    Files extracted from Initaializer

---
+++ src/main/java/com/titans/quiz/titansquizapp/TitansQuizAppApplication.java
@@ -0,0 +1,13 @@
+package com.titans.quiz.titansquizapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TitansQuizAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TitansQuizAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/titans/quiz/titansquizapp/TitansQuizAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.titans.quiz.titansquizapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TitansQuizAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
